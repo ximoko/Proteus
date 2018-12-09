1a86c86579c994a5d0bf2ed35ef89270a73eb7dd
Ximoko <joaquinfont2017@cev.com>
2018-12-09 19:50:47 +0100

    menu reedited

---
+++ Assets/Script/Load3D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load3D : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("2", LoadSceneMode.Additive);
+    }
+}
