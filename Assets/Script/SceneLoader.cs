911a8eddfc436e10f20f95f8acb0b61cec03e7b3
Ximoko <joaquinfont2017@cev.com>
2018-12-09 20:55:28 +0100

    Menú linked to 3dScene

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+public class SceneLoader : MonoBehaviour
+{
+    private int scenceName;
+
+    public void Changemenuscene(string sceneName)
+    {
+        Application.LoadLevel(2);
+    }
+}
