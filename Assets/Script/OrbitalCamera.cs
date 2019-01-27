1baf8db4c46e682b92caa3c1aa99fe9b1f845e95
ximoko <43649886+ximoko@users.noreply.github.com>
2019-01-27 11:53:26 +0100

    OrbitalCamera added

---
+++ Assets/Script/OrbitalCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalCamera : MonoBehaviour {
+
+    public float speed = 8f;
+    public float distance = 3f;
+
+    public Transform target;
+
+    private Vector2 input;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("o"))
+        {
+            input += new Vector2(Input.GetAxis("Mouse X") * speed, Input.GetAxis("Mouse Y") * speed);
+            
+            transform.localRotation = Quaternion.Euler(input.y, input.x, 0); ;
+            transform.localPosition = target.position - (transform.localRotation * Vector3.forward * distance);
+        }
+    }
+}
