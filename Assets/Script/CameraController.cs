1cb492d33a7793af4f9a8c8f3ae51f62934d5bf8
ximoko <43649886+ximoko@users.noreply.github.com>
2019-01-29 16:49:14 +0100

    CameraController OK

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -2,37 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+    private Camera cam;
+    private Transform targetTransform;
+    private Transform cameraTransform;
 
-    public float panSpeed = 20f;
-    public float panBorderThickness = 10f;
-    public Vector2 panLimit;
+    [Header("Permissions")]
+    public bool isPanning;
+    public bool isZoom;
+    public bool isRotate;
 
+    [Header("Panning")]
+    public Vector3 startPanning;
+    public Vector3 offsetPanning;
+
+    [Header("Zoom")]
+    public float minZoom;
+    public float maxZoom;
+    public float zoom;
+    private float scroll;
+    public float scrollDistance = 0.1f;
+
+    [Header("Rotation")]
+    public Vector2 mouseAxis;
+    public float sensitiveRot = 2.0f;
+	
+	void Start ()
+    {
+        cam = GetComponentInChildren<Camera>();
+        cameraTransform = cam.transform;
+		targetTransform = transform;
+
+        zoom = targetTransform.position.z - cameraTransform.position.z;
+	}
+	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if(!isPanning) Orbital();
+        Zoom();
+        if (!isRotate) Panning();
+	}
 
-        Vector3 pos = transform.position;
+    void Panning()
+    {
+        Vector3 worldPoint = Input.mousePosition;
+        worldPoint.z = zoom * 1.5f;
 
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height + panBorderThickness)
+        if (Input.GetMouseButtonDown(2))
         {
-            pos.z += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
-        {
-            pos.z -= panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-        {
-            pos.x += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
-        {
-            pos.x -= panSpeed * Time.deltaTime;
-        }
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+            startPanning = cam.ScreenToWorldPoint(worldPoint);
 
-        transform.position = pos;
-
+            isPanning = true;
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            offsetPanning = startPanning - cam.ScreenToWorldPoint(worldPoint);
+            targetTransform.localPosition = targetTransform.localPosition + offsetPanning;
+        }
+        else if (Input.GetMouseButtonUp(2)) isPanning = false;
     }
+
+    void Zoom()
+    {
+        scroll = Input.mouseScrollDelta.y;
+
+        zoom -= scroll * scrollDistance;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+
+        cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, cameraTransform.localPosition.y, -zoom);
+    }
+
+    void Orbital()
+    {
+
+        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetMouseButton(0))
+        {
+            isRotate = true;
+            mouseAxis.x = Input.GetAxis("Mouse X") * sensitiveRot;
+            mouseAxis.y = Input.GetAxis("Mouse Y") * sensitiveRot;
+
+            Vector3 localEulerRot = targetTransform.localEulerAngles;
+
+            localEulerRot.y += mouseAxis.x;
+            localEulerRot.x -= mouseAxis.y;
+
+            targetTransform.localRotation = Quaternion.Euler(localEulerRot);
+        }
+        if (Input.GetMouseButtonUp(0)) isRotate = false;
+    }
+
 }
